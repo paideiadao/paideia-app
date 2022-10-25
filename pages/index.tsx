87460787be209dd7a6c8c76aecadf50e6655c759
esot321c <esot321c@gmail.com>
2022-10-24 20:10:24 -0700

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import { NextPage } from "next";
-import React, { useEffect, useState, useContext } from "react";
-import { useRouter } from "next/router";
-import { SlugContext } from "@contexts/SlugContext";
+import React from "react";
 
 interface IDaoProps {}
 
-const Dao: NextPage<IDaoProps> = ({}) => {
-  const slugContext = useContext(SlugContext);
-  // routing
-  const router = useRouter();
-  const { dao } = router.query;
-  console.log(slugContext.daoSlugs);
+const Dao: NextPage<IDaoProps> = () => {
   return <>No DAO loaded.</>;
 };
 
