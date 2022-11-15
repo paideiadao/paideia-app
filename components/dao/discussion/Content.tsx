900eeca1291c43859c2eb9b1f27f6ed973ee2878
esot321c <esot321c@gmail.com>
2022-11-15 11:27:18 -0800

    remove tinymce and use markdown editor

--- components/dao/discussion/Content.tsx
+++ components/dao/discussion/Content.tsx
@@ -3,14 +3,21 @@ import TextEditor from "@components/utilities/TextEditor";
 import DiscussionContext, {
   IDiscussionContext,
 } from "@lib/dao/discussion/DiscussionContext";
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import React, { useState, useCallback } from "react";
-import SimpleMdeReact from "react-simplemde-editor";
+// import SimpleMdeReact from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+
+const SimpleMdeEditor = dynamic(
+  () => import("react-simplemde-editor"),
+  { ssr: false }
+);
 
 const Content: React.FC = () => {
+  const theme = useTheme()
   const context = React.useContext<IDiscussionContext>(DiscussionContext);
-  
+
   return (
     <Box
       sx={{
@@ -26,13 +33,56 @@ const Content: React.FC = () => {
           subtitle="Write about your discussion, you can add videos, links, images, code snippets, and format your content using the editor below."
         />
       </Box>
+      <Box
+        sx={
+          theme.palette.mode === 'dark' &&
+          {
+            '& .EasyMDEContainer': {
+              '& .editor-toolbar': {
+                borderColor: 'rgba(133, 133, 133, 0.2)',
+              },
+              '& .separator': {
+                borderColor: 'rgba(133, 133, 133, 0.2)',
+              },
+              '& button': {
+                color: theme.palette.text.secondary,
+                background: '',
+                '& :hover': {
+                  background: theme.palette.background.paper
+                }
+              },
+              '& button.active': {
+                background: theme.palette.background.paper,
+                '& :hover': {
+                  background: theme.palette.background.paper
+                }
+              },
+              '& .CodeMirror': {
+                color: theme.palette.text.primary,
+                background: 'rgba(0,0,0,0)',
+                borderColor: 'rgba(133, 133, 133, 0.2)',
+              },
+              '& .CodeMirror-focused': {
+                borderColor: theme.palette.primary.main,
+              },
+              '& .CodeMirror-cursor': {
+                color: theme.palette.text.secondary,
+              },
+              '& .editor-preview': {
+                background: theme.palette.background.paper,
+              }
 
-      <SimpleMdeReact
-        value={context.api.value.content}
-        onChange={(value: any) =>
-          context.api.setValue({ ...context.api.value, content: value })
+            }
+          }
         }
-      />;
+      >
+        <SimpleMdeEditor
+          value={context.api.value.content}
+          onChange={(value: any) =>
+            context.api.setValue({ ...context.api.value, content: value })
+          }
+        />
+      </Box>
       {/* <TextEditor
         onChange={(value: any) =>
           context.api.setValue({ ...context.api.value, content: value })
